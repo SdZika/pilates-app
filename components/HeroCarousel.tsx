fa3f8dac56beed103af50a766e9f8a6718f4549f
SdZika <markozivkovic1982@gmail.com>
2025-08-11 23:33:45 +0200

    add link to heroCarousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -3,11 +3,13 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 interface Slide {
   title: string;
   description: string;
   image: string;
+  learnMore: string;
 }
 
 export default function HeroCarousel() {
@@ -23,7 +25,7 @@ export default function HeroCarousel() {
     return () => clearInterval(interval);
   }, [carouselSlides.length]);
 
-  const { title, description, image } = carouselSlides[current];
+  const { title, description, image, learnMore } = carouselSlides[current];
 
   return (
     <div className="relative w-full h-[500px] md:h-[600px] lg:h-[700px] overflow-hidden">
@@ -44,6 +46,12 @@ export default function HeroCarousel() {
           <p className="text-lg md:text-xl mb-6 drop-shadow-sm">
             {description}
           </p>
+          <Link
+            href="/about"
+            className="inline-block px-5 py-2 bg-gradient-to-r from-pink-500 to-purple-600 hover:from-pink-600 hover:to-purple-700 rounded-full font-medium text-white shadow-lg"
+          >
+            {learnMore}
+          </Link>
         </div>
       </div>
 
